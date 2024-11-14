8fb1273c8cc71c30b8581dbc39e9c035132ca5b2
Marcus Sjöblom <marsj402@student.liu.se>
2024-11-14 13:35:28 +0100

    Added files

--- Lab4/Lab4part2.py
+++ Lab4/Lab4part2.py
@@ -1,21 +1,24 @@
 import sklearn
+import os
 from sklearn.datasets import load_files
 
-moviedir = r'D:\Lab\nltk_data\corpora\movie_reviews'
+# Define the path relative to the script location
+current_directory = os.path.dirname(os.path.abspath(__file__))
+moviedir = os.path.join(current_directory, 'movie_reviews')
 
 # loading all files. 
 movie = load_files(moviedir, shuffle=True)
 
-len(movie.data)
+print(len(movie.data))
 
 # target names ("classes") are automatically generated from subfolder names
-movie.target_names
+print(movie.target_names)
 
 # First file seems to be about a Schwarzenegger movie. 
-movie.data[0][:500]
+print(movie.data[0][:500])
 
 # first file is in "neg" folder
-movie.filenames[0]
+print(movie.filenames[0])
 
 # first file is a negative review and is mapped to 0 index 'neg' in target_names
-movie.target[0]
+print(movie.target[0])
